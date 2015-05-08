59a4210a878fffc415621619ce3495cbd4db319d
Elliott Asher <elliott250@gmail.com>
2015-05-08 16:00:48 -0400

    inital file structure complete

--- app/views/view1/view1.js
+++ app/views/view1/view1.js
@@ -3,8 +3,8 @@
 angular.module('myApp.view1', ['ngRoute'])
 
 .config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {
-    templateUrl: 'view1/view1.html',
+  $routeProvider.when('/views/view1', {
+    templateUrl: 'views/view1/view1.html',
     controller: 'View1Ctrl'
   });
 }])
