30ebc378eee452be11392fc11c4d697f89954ee3
Elliott Asher <elliott250@gmail.com>
2015-07-09 18:09:51 -0400

    got d3 code working finally

--- app/views/algorithms/linear_search/linear_search_view.js
+++ app/views/algorithms/linear_search/linear_search_view.js
@@ -17,10 +17,7 @@ angular.module('myApp.linear_search', ['ngRoute','d3'])
         d3Service.d3().then(function(d3) {
  
           var renderTimeout;
-          var margin = parseInt(attrs.margin) || 20,
-              barHeight = parseInt(attrs.barHeight) || 20,
-              barPadding = parseInt(attrs.barPadding) || 5;
- 
+
           var svg = d3.select(ele[0])
             .append('svg')
             .style('width', '100%');
@@ -29,13 +26,8 @@ angular.module('myApp.linear_search', ['ngRoute','d3'])
             scope.$apply();
           };
 
-                    // hard-code data
-          scope.data = [
-            {name: "Greg", score: 98},
-            {name: "Ari", score: 96},
-            {name: 'Q', score: 75},
-            {name: "Loser", score: 48}
-          ];
+          // hard-code data
+          scope.data = [4, 8, 15, 16, 23, 42, 33];
  
           scope.$watch(function() {
             return angular.element($window)[0].innerWidth;
@@ -52,57 +44,38 @@ angular.module('myApp.linear_search', ['ngRoute','d3'])
  			
             if (!data) return;
             if (renderTimeout) clearTimeout(renderTimeout);
- 
             renderTimeout = $timeout(function() {
-              var width = d3.select(ele[0])[0][0].offsetWidth - margin,
-                  height = scope.data.length * (barHeight + barPadding),
-                  color = d3.scale.category20(),
-                  xScale = d3.scale.linear()
-                    .domain([0, d3.max(data, function(d) {
-                      return d.score;
-                    })])
-                    .range([0, width]);
- 
-              svg.attr('height', height);
- 
-              svg.selectAll('rect')
-                .data(data)
-                .enter()
-                  .append('rect')
-                  .on('click', function(d,i) {
-                    return scope.onClick({item: d});
-                  })
-                  .attr('height', barHeight)
-                  .attr('width', 140)
-                  .attr('x', Math.round(margin/2))
-                  .attr('y', function(d,i) {
-                    return i * (barHeight + barPadding);
-                  })
-                  .attr('fill', function(d) {
-                    return color(d.score);
-                  })
-                  .transition()
-                    .duration(1000)
-                    .attr('width', function(d) {
-                      return xScale(d.score);
-                    });
-              svg.selectAll('text')
-                .data(data)
-                .enter()
-                  .append('text')
-                  .attr('fill', '#fff')
-                  .attr('y', function(d,i) {
-                    return i * (barHeight + barPadding) + 15;
-                  })
-                  .attr('x', 15)
-                  .text(function(d) {
-                    return d.name + " (scored: " + d.score + ")";
-                  });
+              
+
+                  
+                  var width = 320,
+                  barHeight = 20;
+                  
+                  //var barHeight = d3.select(ele[0])[0][0].offsetWidth - margin,
+                  //width = scope.data.length * (barWidth + barPadding);
+                  var x = d3.scale.linear()
+                        .domain([0,d3.max(data)])
+                        .range([0,width]);
+
+                  var chart = svg.attr("width",barHeight * data.length)
+                                 .attr("height",width);
+
+                  var bar = chart.selectAll("g")
+                           .data(data)
+                           .enter().append("g")
+
+                  bar.append("rect")
+                    .attr("width",barHeight-1)
+                    .attr("height",function(d) {return x(d);})
+                    .attr("x",function(d,i){return i*barHeight-1})
+                    .attr("y",function(d,i){return width-x(d)});
+
             }, 200);
           };
         });
       }}
 }])
+
 .controller('linearCtrl', [function() {
 
 }]);
