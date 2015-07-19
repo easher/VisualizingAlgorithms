729a512d1749eb24b3a346d46d8c4d27baf60788
Elliott Asher <elliottasher@Elliotts-MacBook-Pro.local>
2015-07-19 12:04:24 -0400

    fixed spacing

--- app/views/algorithms/linear_search/linear_search_view.js
+++ app/views/algorithms/linear_search/linear_search_view.js
@@ -3,63 +3,79 @@
 angular.module('myApp.linear_search', ['ngRoute','d3','d3BarChart'])
 
 .config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/views/algorithms/linear_search', {
-    templateUrl: 'views/algorithms/linear_search/linear_search.html',
-    controller: 'linearCtrl',
-    directive: 'd3Bars'
-  });
+	$routeProvider.when('/views/algorithms/linear_search', {
+    		templateUrl: 'views/algorithms/linear_search/linear_search.html',
+    		controller: 'linearCtrl',
+    		directive: 'd3Bars'
+  	});
 }])
 
 .controller('linearCtrl', ['$scope',function($scope) {
-  $scope.data = [4, 8, 15, 16, 23, 42, 33];
-  $scope.finddata = {val:0};
-  $scope.change = function(selected) { 
-  $scope.finddata.val = selected
-  }
+	$scope.data = [4, 8, 15, 16, 23, 42, 33];
+	$scope.finddata = {val:0};
+	$scope.change = function(selected) { 
+		$scope.finddata.val = selected
+	}
 }])
 
 .directive('d3Bars', ['$window', '$timeout', 'd3Service','d3BarChartUtil', 
-  function($window, $timeout, d3Service,d3BarChartUtil) {
-	return {
-      		restrict: 'A',
+	function($window, $timeout, d3Service,d3BarChartUtil) {
+		return {
+      			restrict: 'A',
       		
-          scope: {
-        		'finddata': '=',
-			      'data': '='
-      		},
-      		link: function(scope, ele, attrs) {
-         		d3Service.d3().then(function(d3) {
+          		scope: {
+        			'finddata': '=',
+			      	'data': '='
+      			},
+      			
+			link: function(scope, ele, attrs) {
+         			d3Service.d3().then(function(d3) {
  
-         		var renderTimeout;
+         			var renderTimeout;
 
  
-            var svg = d3BarChartUtil.declareSVG(ele);
+            			var svg = d3BarChartUtil.declareSVG(ele);
          		
-            $window.onresize = function() {
-         			scope.$apply();
-         		};
+            			$window.onresize = function() {
+         				scope.$apply();
+         			};
 
-          		scope.$watch(function() {
-				        return angular.element($window)[0].innerWidth;
-          		}, 
+          			scope.$watch(function() {
+					return angular.element($window)[0].innerWidth;
+          			}, 
 	  		
-			function() {
-		  		scope.render(scope.data);
-      });
+					function() {
+		  	 		       scope.render(scope.data);
+            				}
+				);
 
-	 		scope.$watch('finddata',function(newValue,oldValue) {
-	  		
-			}, true);	  
+	 		      	scope.$watch('finddata',function(newValue,oldValue) {
+	  		       	
+					var transition = svg.transition().duration(10000);
+               				var delay = function(d, i) { return i * 50; };
+               				var allG = transition.selectAll("g").delay(delay);
+               
+               				allG.each(function(d,i){
+
+                    				if(d == newValue.val){
+                      					d3.select(this).attr("fill","green")
+                    				} else {
+                      					d3.select(this).attr("fill","blue");
+                    				} 
+              				});
+            
+           			}, true);	  
           
-	  		scope.render = function(data) {
+	  			scope.render = function(data) {
           			
-            			if (!data) {console.log("hello");return;}
-            			if (renderTimeout) clearTimeout(renderTimeout);
+            				if (!data) {console.log("hello");return;}
+            				if (renderTimeout) clearTimeout(renderTimeout);
 
-                  var width = 320, barHeight = 20;
-                  d3BarChartUtil.drawBarChart(svg,data,width,barHeight);
+                  			var width = 320, barHeight = 20;
+                  			d3BarChartUtil.drawBarChart(svg,data,width,barHeight);
                  
-              };
-        	});
-      	}}
+              			};
+        			});
+      			}
+		}
 }]);
